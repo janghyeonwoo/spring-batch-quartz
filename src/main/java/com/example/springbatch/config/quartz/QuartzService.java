3b54c9980f04cafb99f8bf1bf19d40a36236e454
SMART-TN-038 <blue672@naver.com>
2022-07-01 09:06:01 +0900

    쿼츠 설정 - jobListener - TriggerListener - Job created - Quartz configuration

--- src/main/java/com/example/springbatch/config/quartz/QuartzService.java
+++ src/main/java/com/example/springbatch/config/quartz/QuartzService.java
@@ -32,7 +32,7 @@ public class QuartzService {
             Map paramsMap = new HashMap<>();
             paramsMap.put("executeCount", 1);
             paramsMap.put("date", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-            addJob(QuartzJob.class, "QuartzJob2", "Quartz 잡", paramsMap, "0/5 * * * * ?");
+            addJob(QuartzJob.class, "QuartzJob", "Quartz Job 입니다", paramsMap, "0/1 * * * * ?");
 
         } catch (Exception e){
             log.error("addJob error  : {}", e);
@@ -40,7 +40,7 @@ public class QuartzService {
         }
     }
 
-
+    //Job 추가
     public <T extends Job> void addJob(Class<? extends Job> job ,String name, String  dsec, Map paramsMap, String cron) throws SchedulerException {
         JobDetail jobDetail = buildJobDetail(job,name,dsec,paramsMap);
         Trigger trigger = buildCronTrigger(cron);
@@ -48,6 +48,8 @@ public class QuartzService {
         scheduler.scheduleJob(jobDetail,trigger);
     }
 
+
+    //JobDetail 생성
     public <T extends Job> JobDetail buildJobDetail(Class<? extends Job> job, String name, String desc, Map paramsMap) {
         JobDataMap jobDataMap = new JobDataMap();
         jobDataMap.putAll(paramsMap);
@@ -60,6 +62,7 @@ public class QuartzService {
                 .build();
     }
 
+    //Trigger 생성
     private Trigger buildCronTrigger(String cronExp) {
         return TriggerBuilder.newTrigger()
                 .withSchedule(CronScheduleBuilder.cronSchedule(cronExp))
